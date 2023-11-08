754453cff17b672f3c999cd1b2d478e8819e6e2f
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-07 19:24:42 -0500

    Add functionality to slotmachines

--- Casino Simulation/SlotMachines.java
+++ Casino Simulation/SlotMachines.java
@@ -1,5 +1,6 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.*;
+import java.util.Random;
 /**
  * Write a description of class SlotMachines here.
  * 
@@ -8,12 +9,56 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  */
 public class SlotMachines extends Game
 {
-    /**
-     * Act - do whatever the SlotMachines wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    public void act()
-    {
-        // Add your action code here.
+    private int numberOfReels;
+    private Random random;
+    private int[] result;
+    private boolean jackpot;
+    
+    public SlotMachines(){
+        numberOfReels = 3;
+        jackpot = false;
+    }
+    
+    public void act(){
+        if(isGamblerInFront()){
+            stationGambler();
+        }
+    }
+    
+    private boolean isGamblerInFront(){
+        List<Gambler> gamblers = getObjectsInRange(50, Gambler.class); // Adjust range as needed
+        return !gamblers.isEmpty();
+    }  
+    
+    
+    public void stationGambler(){
+        // code here for stationing the gambler in front of the slot machine
+        
+        spinReels();
+    }
+    
+    // spin reels and determine outcome
+    public void spinReels(){
+        result = new int[numberOfReels];
+        
+        // spin each reel
+        for (int i = 0; i < numberOfReels; i++) {
+            result[i] = random.nextInt(7)+1; //random num between 1 and 7
+        }
+        
+        checkIfWin();
+    }
+
+    // calculate payout based on spin outcome
+    public boolean checkIfWin(){
+        for(int i = 1; i < result.length; i++){
+            if(result[i] != result[i-1]){
+                jackpot = false;
+            }
+            else{
+                jackpot = true;
+            }
+        }
+        return jackpot;
     }
 }
