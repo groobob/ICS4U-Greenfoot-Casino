61bd31b0d3c06d93774acf5785496c3f5ea0046b
1Jimz <JimmyJunjiZhu@gmail.com>
2023-11-11 18:52:43 -0500

    Implementation of ImageManager, SoundManager, and Gambler's pathing.
    Implemented:
    -ImageManager
    -SoundManager
    -Gambler's pathing
    
    Also added lots of testing and debug code.

--- Casino Simulation/CasinoWorld.java
+++ Casino Simulation/CasinoWorld.java
@@ -1,22 +1,44 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
-/**
- * Write a description of class MyWorld here.
- * 
- * @author (your name) 
- * @version (a version number or a date)
- */
+import greenfoot.*;
+import java.util.*;
 public class CasinoWorld extends World
 {
-
-    /**
-     * Constructor for objects of class MyWorld.
-     * 
-     */
+    int delay=1;//temp
+    public class pos{//temp
+        int x,y;
+        pos(int x, int y){
+            this.x=x;this.y=y;
+        }
+    }
+    List<pos> tempPlaces = new ArrayList<pos>();//temp
     public CasinoWorld()
     {    
-        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(1200, 740, 1, false);
-        
+        super(1200, 740, 1, false); 
+        Greenfoot.setSpeed(50);
+        tempPlaces.add(new pos(450,200));
+        tempPlaces.add(new pos(350,200));
+        tempPlaces.add(new pos(250,200));
+        tempPlaces.add(new pos(150,200));
+        tempPlaces.add(new pos(50,200));
+        tempPlaces.add(new pos(450,400));
+        tempPlaces.add(new pos(350,400));
+        tempPlaces.add(new pos(250,400));
+        tempPlaces.add(new pos(150,400));
+        tempPlaces.add(new pos(50,400));
+        //tempPlaces.add(new pos(300,200));
+        //tempPlaces.add(new pos(300,200));
+        //addObject(new DarkTint(),200,400);
+        //addObject(new Player(),600,400);
+        addObject(new Entrance(),600,600);
+        for(pos p: tempPlaces)addObject(new Thing(),p.x,p.y);
+        //addObject(new Gambler(300,200),(Greenfoot.getRandomNumber(2)==0?1250:-50),700+(Greenfoot.getRandomNumber(2)==0?-Greenfoot.getRandomNumber(40):Greenfoot.getRandomNumber(40)));
+        //setPaintOrder(Player.class, Light.class);
+        //SoundManager.addSound(50,"A","wav");
+    }
+    public void act(){
+        if(--delay==0){
+            delay=Greenfoot.getRandomNumber(60)+30;
+            pos p = tempPlaces.get(Greenfoot.getRandomNumber(tempPlaces.size()));
+            addObject(new Gambler(p.x,p.y+50),(Greenfoot.getRandomNumber(2)==0?1250:-50),700+(Greenfoot.getRandomNumber(2)==0?-Greenfoot.getRandomNumber(40):Greenfoot.getRandomNumber(40)));
+        }
     }
 }
--- Casino Simulation/Gambler.java
+++ Casino Simulation/Gambler.java
@@ -1,19 +1,52 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.*;
 /**
- * Write a description of class Gambler here.
+ * Gambler.
+ * @author Jimmy Zhu
+ * @version 1111
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
+//DO NOT ABSTRACT. Not abstracted rn because need this needs to work as a base.
 public class Gambler extends Actor
 {
-    /**
-     * Act - do whatever the Gambler wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
+    //in: tx:entrance x, fx:game x, ty: game y
+    //out: tx: entrace x, fx: out world x, fy: sidewalk y
+    private final int varyRange=40;//temp
+    private final int entranceX=600+(Greenfoot.getRandomNumber(2)==0?-Greenfoot.getRandomNumber(varyRange):Greenfoot.getRandomNumber(varyRange));//temp
+    private final int sidewalkY=700+(Greenfoot.getRandomNumber(2)==0?-Greenfoot.getRandomNumber(varyRange):Greenfoot.getRandomNumber(varyRange));//temp
+    private final int outMapX=(Greenfoot.getRandomNumber(2)==0?1250:-50);//temp
+    private int speed=5,tx=entranceX,fx,ty;
+    private boolean stopped=false,flag=false,down=false;
+    public Gambler(int fx, int ty){
+        this.fx=fx;
+        this.ty=ty;
+    }
     public void act()
     {
-
+        if(!stopped){
+            if(Math.abs(tx-getX())>5)setLocation(getX()+speed*Integer.signum(tx-getX()),getY());
+            else if(Math.abs(ty-getY())>5)setLocation(getX(),getY()+speed*Integer.signum(ty-getY()));
+            else if(tx!=fx)tx=fx;
+            else if(tx==1250||tx==-50)getWorld().removeObject(this);
+            else if(!flag){
+                if(!down)ty-=50;
+                else ty+=50;
+                flag=true;
+            }
+            else if(!down){
+                stopped=true;
+                down=true;
+                flag=false;
+                //station
+                unstop();//temp
+            }
+            else{
+                tx=entranceX;
+                ty=sidewalkY;
+                fx=outMapX;
+            }
+        }
+    }
+    public void unstop(){//temp
+        stopped=false;
     }
 }
