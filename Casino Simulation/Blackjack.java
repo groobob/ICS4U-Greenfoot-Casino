52d25e3ea78621990a6dbae0071c9557e914b3a5
Richard <elitegem84@gmail.com>
2023-11-14 14:24:57 -0500

    Squashed commit of the following:
    commit 10a38a02f4862171fdfcc91038bd9ae7b70216e8
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Tue Nov 14 14:15:09 2023 -0500
    
        Roulette and Poker changes
    
    commit c84ddd27617a014a5d1222774e251124e2d04cd2
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Tue Nov 14 14:02:37 2023 -0500
    
        roulette minor changes
    
    commit 634352b76614fbc507df6bc78f50a5a3e3645aaf
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Mon Nov 13 22:35:21 2023 -0500
    
        Roulette 1.0
    
    commit 8a99c9d93b747ae2c881628fabb72a91138465e3
    Merge: 8367a27 87dc6e4
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Mon Nov 13 12:41:02 2023 -0500
    
        Merge branch 'main' into David
    
    commit 8367a27e19f1a3bbed1886f989bd7bc6e48ab214
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Mon Nov 13 12:39:50 2023 -0500
    
        Import ArrayList
    
    commit 2c92e21eae8d4f02c08be3ba64644d2759be7438
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Mon Nov 13 12:24:35 2023 -0500
    
        Minor changes to my game classes
    
    commit e08643c5d16ea38904a3ae10ce2c71501b266cd9
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Thu Nov 9 18:37:26 2023 -0500
    
        Roulette Version 0.1
    
    commit 1bd3e83dcc36ac2b06f83756493fbce116794f7a
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Thu Nov 9 18:07:17 2023 -0500
    
        Abstract Game and Progress on Roulette
    
    commit 0855bdc18b203ce9235c8b7a301b1cd2bdbfecb2
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Thu Nov 9 17:56:18 2023 -0500
    
        Added Subclasses
    
    commit bfc66331e07e7fb495b07d4f54f546eabe59da9e
    Author: SoulEmp <Awesomez0legendz@gmail.com>
    Date:   Wed Nov 8 13:43:01 2023 -0500
    
        Update project.greenfoot

---
+++ Casino Simulation/Blackjack.java
@@ -0,0 +1,40 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;  // (ArrayList)
+
+/**
+ * 
+ * 
+ * <p>=============== Experimental Data ===============</p>
+ * 
+ * 11/10/2023 <br>
+ * Played: ~20 rounds of blackjack (40 chips) with Mr. Cohen <br>
+ * Result: Cohen: 40 chips, House: 0 chips <br>
+ * <br>
+ * 11/10/2023 <br>
+ * Played: 3 rounds of blackjack (40 chips) with Liyu <br>
+ * Result: Liyu: 0 chips, House: 40 chips <br>
+ * <br>
+ * =================================================
+ * 
+ * @author David Guo
+ * @version 0.1 11/10/2023
+ */
+public class Blackjack extends Game
+{
+    private int trueCount;
+    public Blackjack(){
+        
+    }
+    /**
+     * Act - do whatever the Blackjack wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+    
+    private int calculateOdds(){
+        return 0;
+    }
+}
--- Casino Simulation/Game.java
+++ Casino Simulation/Game.java
@@ -1,13 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 /**
- * Write a description of class Game here.
+ * The superclass
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author David Guo
+ * @version 1.0 11/13/2023
  */
 public class Game extends Actor
 {
+    // Array of all gamblers currently playing
+    protected Gambler[] gamblersPlaying;
     /**
      * Act - do whatever the Game wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,5 +17,10 @@ public class Game extends Actor
     public void act()
     {
         // Add your action code here.
+        
+    }
+    
+    public void stationGambler(){
+        
     }
 }
