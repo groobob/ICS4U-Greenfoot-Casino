3df73265a63bafe3a621a9ea347eb1da134fa02f
SoulEmp <Awesomez0legendz@gmail.com>
2023-11-20 14:03:08 -0500

    Undo merge

--- Casino Simulation/ImageManager.java
+++ Casino Simulation/ImageManager.java
@@ -1,30 +1,45 @@
+import greenfoot.*;
+import java.util.*;
 /**
- * Write a description of class ImageManager here.
+ * Class that manages images.
+ * Recommended to add images in world constructor.
+ * @author Jimmy Zhu
+ * @version 1110
  * 
- * @author (your name) 
- * @version (a version number or a date)
  */
 public class ImageManager  
 {
-    // instance variables - replace the example below with your own
-    private int x;
-
+    //using 1D over 2D because according to a StackOverFlow post 2D takes more memory
+    private static Map<String,GreenfootImage[]> images = new HashMap<String,GreenfootImage[]>();
     /**
-     * Constructor for objects of class ImageManager
+     *adds images denoted.
      */
-    public ImageManager()
-    {
+    //for method to not error make sure the name is in formatted(name+types+"_"+frames+".png").
+    public static void addImages(String name, int types, int frames){
+        GreenfootImage temp[] = new GreenfootImage[types*frames+1];
+        for(int i = 0; i<types; i++){
+            for(int j = 1; j<=frames; j++)temp[i*frames+j]=new GreenfootImage(name+types+"_"+frames+".png");
+        }
+        images.put(name,temp);
     }
-
     /**
-     * An example of a method - replace this comment with your own
-     * 
-     * @param  y   a sample parameter for a method
-     * @return     the sum of x and y 
+     *adds images denoted.
      */
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return x + y;
+    public static void addImages(String name, int frames){
+        GreenfootImage temp[] = new GreenfootImage[frames+1];
+        for(int i = 1; i<=frames; i++)temp[frames+i]=new GreenfootImage(name+frames+".png");
+        images.put(name,temp);
+    }
+    /**
+     *gets image denoted.
+     */
+    public static GreenfootImage getImage(String name, int types, int frames){
+        return images.get(name)[types*frames];
+    }
+    /**
+     *gets image denoted.
+     */
+    public static GreenfootImage getImage(String name, int frames){
+        return images.get(name)[frames];
     }
 }
--- Casino Simulation/SlotMachines.java
+++ Casino Simulation/SlotMachines.java
@@ -8,74 +8,37 @@ import java.util.Random;
  * @version (a version number or a date)
  */
 public class SlotMachines extends Game {
+    private int tempGamingTime;
     private int numberOfReels;
     private int[] result;
     private boolean jackpot;
     private int moneyWon;
     private int moneyLost;
-    private Gambler gambler; 
-
-    private boolean isOccupied = false;
-    
-    public SlotMachines() {
+    public SlotMachines(SeatManager.Seat[] seats) {
+        super(seats);
         numberOfReels = 3;
         jackpot = false;
         moneyWon = Greenfoot.getRandomNumber(99)+1;
         moneyLost = Greenfoot.getRandomNumber(50)+1;
         result = new int[numberOfReels];
     }
-    
-    public boolean isOccupied() {
-        return isOccupied;
-    }
-    public void setOccupied(boolean occupied) {
-        isOccupied = occupied;
-    }
-
-    public void spinReels() {
-        if (!isOccupied) {
-            setOccupied(true);
-            
-            gamblerPay();
-            for (int i = 0; i < numberOfReels; i++) {
-                result[i] = Greenfoot.getRandomNumber(7); // random num 1-7
-            }
-    
-            checkIfWin();
-            if (gambler != null) {
-                gambler.setPlayingSlot(false); // reset gambler state
-            }
-            releaseSlot();
-        }
-    }
-    
     public void gamblerPay(){
-        Gambler.playMoneyEffect(gambler, false, moneyLost);
+        gamblers[0].playMoneyEffect(gamblers[0], false, moneyLost);
     }
-    
-    public void releaseSlot() {
-        setOccupied(false);
-        if (gambler != null) {
-            gambler.setPlayingSlot(false);
-            Gambler.unstop();
+    public void act(){
+        //rn for testing gambler dips immediately
+        if(gamblers[0]!=null&&gamblers[0].isPlaying()){
+            gamblers[0].stopPlaying();
+            gamblers[0]=null;
         }
     }
-    
-    public void assignGambler(Gambler gambler) {
-        this.gambler = gambler;
-        if (gambler != null) {
-            gambler.setPlayingSlot(true); // change gambler state to playing
-        }
-    }
-
     public boolean checkIfWin() {
         System.out.println("1222222testttttttt");
-
         for (int i = 1; i < result.length; i++) {
             if (result[i] != result[i - 1]) {
                 jackpot = false;
-                if (gambler != null) {
-                    Gambler.playMoneyEffect(gambler, true, moneyWon);
+                if (gamblers[0] != null) {
+                    gamblers[0].playMoneyEffect(gamblers[0], true, moneyWon);
                 }
             } else {
                 jackpot = true;
