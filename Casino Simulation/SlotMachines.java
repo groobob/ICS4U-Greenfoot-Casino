948ad8d40a2cf99816c17a84e057497ff276d483
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-20 23:26:00 -0500

    finalize slot machine

--- Casino Simulation/SlotMachines.java
+++ Casino Simulation/SlotMachines.java
@@ -1,58 +1,66 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-/**
- * Author: Dorsa
- */
+
 public class SlotMachines extends Game {
-    private static final int cost = 5; // cost of a bet for the gambler
-    private static final int minWinAmount = 100; // min amount of money gamblers can win by slots
-    private static final int maxWinAmount = 500; // max amount of money gamblers can win by slots
-    private static final int delay = 20; // delay time in act cycles
-    private int delayCounter; // counter for the delay
+    private static final int cost = 5;
+    private static final int minWinAmount = 100;
+    private static final int maxWinAmount = 500;
+    private static final int delay = 20;
+    private int maxPlays = Greenfoot.getRandomNumber(5)+3;
+    private int delayCounter;
+    private int playCounter; // Counter for number of plays
 
     public SlotMachines(SeatManager.Seat[] seats) {
         super(seats);
         delayCounter = delay;
+        playCounter = 0; // Initialize play counter
     }
-    
+
     public void act() {
         playGameCycle();
     }
 
     private void playGameCycle() {
         if (isGamblerAvailable()) {
+            spinReels();
             if (delayCounter >= delay) {
-                // starts a new game cycle
                 deductGameCost();
-                checkWinAndAwardPrize();
-                delayCounter = 0; // resets delay counter for the next game
+            
+                delayCounter = 0;
+
+                playCounter++; // increment play counter after each play
+                if (playCounter >= maxPlays) {
+                    endGamblerSession(); // end session after 10 plays
+                }
             } else {
                 delayCounter++;
             }
         } else {
-            // resets game for a new gambler
-            delayCounter = delay;
+            resetForNewGambler();
         }
     }
+
     private void deductGameCost() {
         if (isGamblerAvailable()) {
             gamblers[0].playMoneyEffect(gamblers[0], false, cost);
         }
     }
 
-    private void checkWinAndAwardPrize() {
-        if (isGamblerAvailable() && Greenfoot.getRandomNumber(2)==0) {
+    private void spinReels() {
+        if (isGamblerAvailable() && Greenfoot.getRandomNumber(2) == 0) {
             int winAmount = minWinAmount + Greenfoot.getRandomNumber(maxWinAmount - minWinAmount + 1);
-            gamblers[0].playMoneyEffect(gamblers[0], true, winAmount);
+            gamblers[0].playMoneyEffect(gamblers[0], Greenfoot.getRandomNumber(2) == 0, winAmount);
         }
-    }
-    
-    private boolean decidesToContinuePlaying() {
-        return isGamblerAvailable() && Greenfoot.getRandomNumber(2)==0;
     }
 
     private void endGamblerSession() {
         gamblers[0].stopPlaying();
         gamblers[0] = null;
+        playCounter = 0; // Reset play counter for next gambler
+    }
+
+    private void resetForNewGambler() {
+        delayCounter = delay;
+        playCounter = 0; // Reset play counter for new gambler
     }
 
     private boolean isGamblerAvailable() {
