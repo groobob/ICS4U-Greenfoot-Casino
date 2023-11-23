e2c88843cc7842a9a2236814c870525c67d9fead
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-23 00:07:34 -0500

    add sprites + horsebetting (bugs here)

--- Casino Simulation/CasinoWorld.java
+++ Casino Simulation/CasinoWorld.java
@@ -76,10 +76,8 @@ public class CasinoWorld extends World
             progressLevel++;
             
             // add horsebetting
-            addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(680,312,-20)}),680,312);
-            addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(760,312,-20)}),760,312);
-            addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(840,312,-20)}),840,312);
-            addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(920,312,-20)}),920,312);
+            addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(740,285,-80), new SpotManager.Spot(798,280,-80), new SpotManager.Spot(758+3*30,275,-80), new SpotManager.Spot(758+2*48,255,-80), new SpotManager.Spot(710+48,250,-80), new SpotManager.Spot(710,245,-80), new SpotManager.Spot(678,245,-80)}),781,187);
+           // addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(781,187,-80)}),781,187);
         }
         if(progressLevel==2 && 30 == (HorizontalBar.casinoProfit*100)/casinoTarget){
             numGames++;
@@ -87,6 +85,8 @@ public class CasinoWorld extends World
             progressLevel++;
             
             //add blackjack
+            addObject(new Blackjack(new SpotManager.Spot[]{new SpotManager.Spot(80,445,70), new SpotManager.Spot(130,445,70), new SpotManager.Spot(180,445,70), new SpotManager.Spot(230,445,70), new SpotManager.Spot(270,480,0), new SpotManager.Spot(270,520,0)}),155,500);
+
         }
         if(progressLevel==3 && 40 == (HorizontalBar.casinoProfit*100)/casinoTarget){
             numGames++;
@@ -100,6 +100,8 @@ public class CasinoWorld extends World
             progressLevel++;
             
             // add roulette
+            addObject(new Roulette(new SpotManager.Spot[]{new SpotManager.Spot(1152,241,-120), new SpotManager.Spot(1148,290,-80), new SpotManager.Spot(1116,320,-45), new SpotManager.Spot(1068,335,-30), new SpotManager.Spot(1014,330,-40), new SpotManager.Spot(976,302,-60)}),1060,270);
+        
         }
         if(progressLevel==5 && 60 == (HorizontalBar.casinoProfit*100)/casinoTarget){
             numGames++;
@@ -111,7 +113,8 @@ public class CasinoWorld extends World
             addObject(new SlotMachines(new SpotManager.Spot[]{new SpotManager.Spot(399,245,-20)}),399,245); 
             progressLevel++;
             
-            // add poker
+            // add poker        
+            //addObject(new Poker(new SpotManager.Spot[]{new SpotManager.Spot(920,525,0), new SpotManager.Spot(940,440,55), new SpotManager.Spot(1000,435,50), new SpotManager.Spot(1050,435,50), new SpotManager.Spot(1130,460,75)}),1020,500);
         }
         if(progressLevel==7 && 80 == (HorizontalBar.casinoProfit*100)/casinoTarget){
             numGames++;
@@ -137,7 +140,7 @@ public class CasinoWorld extends World
      */
     private void prepare()
     {
-        
+        // initial slot machines
         addObject(new SlotMachines(new SpotManager.Spot[]{new SpotManager.Spot(58,244,-20)}),58,244);
         addObject(new SlotMachines(new SpotManager.Spot[]{new SpotManager.Spot(57,354,-20)}),57,354);
         
