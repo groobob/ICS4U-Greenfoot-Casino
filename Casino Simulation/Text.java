073fd313f9ca056404bc4071a112393cee7c69cc
1Jimz <JimmyJunjiZhu@gmail.com>
2023-11-24 14:12:27 -0500

    added Text

--- Casino Simulation/Text.java
+++ Casino Simulation/Text.java
@@ -1,20 +1,35 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import greenfoot.*;
 /**
- * Write a description of class Text here.
+ * Text
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @Jimmy Zhu
+ * @1124
  */
 public class Text extends Actor
 {
-    /**
-     * Act - do whatever the Text wants to do. This method is called whenever
-     * the 'Act' or 'Run' button gets pressed in the environment.
-     */
-    
-    public Text(String text, Color color) {
-        GreenfootImage img = new GreenfootImage(text, 24, color, new Color(0, 0, 0, 0));
-        setImage(img);
+    private int type,w,h,s;
+    String font;
+    public Text(int w, int h, int s, String font, String text, int type){
+        this.w=w;
+        this.h=h;
+        this.s=s;
+        this.font=font;
+        setImage(createText(text));
+        this.type=type;
+    }
+    public void act() {
+        switch(type){
+            case 0:setImage(createText("yeah0"));break;
+            case 1:setImage(createText("yeah1"));break;
+            case 2:setImage(createText("yeah2"));break;
+            case 3:setImage(createText("yeah3"));break;
+        }
+    }
+    private GreenfootImage createText(String text){
+        GreenfootImage gfi = new GreenfootImage(w, h);
+        gfi.setColor(Color.BLACK);
+        gfi.setFont(new Font(font, true, false, s/text.length())); 
+        gfi.drawString(text, w/4, h/2);
+        return gfi;
     }
 }
