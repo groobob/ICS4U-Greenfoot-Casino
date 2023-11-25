6d04622bcc95cc8d00b84798cf5bcf8eb604e434
SoulEmp <Awesomez0legendz@gmail.com>
2023-11-24 20:27:17 -0500

    End Screen 0.1

--- Casino Simulation/EndScreen.java
+++ Casino Simulation/EndScreen.java
@@ -3,12 +3,15 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
  * Write a description of class EndScreen here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author David Guo
+ * @version 1.0 11/24/2023
  */
 public class EndScreen extends World
 {
-
+    // Make greenfoot images for all possible endings
+    private GreenfootImage bankruptEnd, wealthEnd;
+    // Sound for both endings
+    private GreenfootSound happyMusic, sadMusic;
     /**
      * Constructor for objects of class EndScreen.
      * 
@@ -16,6 +19,7 @@ public class EndScreen extends World
     public EndScreen()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(CasinoWorld.WIDTH, CasinoWorld.HEIGHT, 1); 
     }
+    
 }
