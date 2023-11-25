da4568b2217375d602bb7544a5616bd8ff7e9030
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-24 22:28:57 -0500

    documentation

--- Casino Simulation/HorseBetting.java
+++ Casino Simulation/HorseBetting.java
@@ -1,12 +1,32 @@
 import greenfoot.*;  // (world, actor, greenfootImage, greenfoot and mouseInfo)
 import java.util.*;
-
 /**
- * Horsebetting game
+ * <html>
+ * <body>
+ * <p><strong>HorseBetting</strong> is a <em>Game</em> subclass that creates and runs the game logic for accurate real-world horse betting.</p>
+ * <p>This class is a part of the Greenfoot game development framework, designed to simulate a horse betting scenario. It manages horse races, including setting up races, accepting gambler bets, and calculating payouts based on the race outcomes.</p>
  * 
- * @author: dorsa
- * @version 1.2 11-23-2023
+ * <h3>Usage:</h3>
+ * <p>To use the <code>HorseBetting</code> class, instantiate it with an array of <code>SpotManager.Spot</code> objects representing the locations in the game. The class automatically handles the logistics of horse racing and interactions with gamblers. Gamblers can place bets on horses, which are processed and paid out based on the results of the races.</p>
+ *
+ * <h3>Version Changes:</h3>
+ * <ul>
+ *   <li>Version 1.2 (11-23-2023): Improved race duration handling and payout calculation accuracy. Fixed minor bugs related to gambler array indexing.</li>
+ * </ul>
+ *
+ * <h3>Additional Information:</h3>
+ * <p>The <code>HorseBetting</code> class is designed for educational purposes within the Greenfoot framework and is not intended for actual betting. It can be extended to include horse statistics, dynamic odds calculation, and sophisticated gambler AI for a more realistic experience.</p>
+ * 
+ * <p><strong>@author :</strong> Dorsa Rohani<br>
+ * <strong>@version :</strong> 1.2<br>
+ * <strong>Date:</strong> 11-24-2023</p>
+ * <h3>Additional Information:</h3>
+ * <p>The <code>HorseBetting</code> class is designed for educational purposes within the Greenfoot framework and is not intended for actual betting. It can be extended to include horse statistics, dynamic odds calculation, and sophisticated gambler AI for a more realistic experience.</p>
+ * 
+ * </body>
+ * </html>
  */
+
 public class HorseBetting extends Game
 {
     private int raceDuration;
@@ -21,6 +41,13 @@ public class HorseBetting extends Game
     private final int evenMultiplier = 2;
     private double betPercentage;
     
+      /**
+       * * <h2>Constructor</h2>
+         * <p><strong>HorseBetting(SpotManager.Spot[] spots)</strong></p>
+         * <ul>
+         *     <li><strong>@param spots</strong> - An array of <code>SpotManager.Spot</code> objects representing the locations for the game.</li>
+         * </ul>
+       */
     public HorseBetting(SpotManager.Spot[] spots) {
         super(spots);
         numberOfHorses = 7; // 7 horses in the race
@@ -29,7 +56,15 @@ public class HorseBetting extends Game
         gamblerSelections = new int[gamblers.length]; 
         gamblerStakes = new int[gamblers.length];
     }
-
+    
+    /**
+     * * <p><strong>void act()</strong></p>
+ * <ul>
+ *     <li>No parameters.</li>
+ *     <li>No return value.</li>
+ *     <li>This method conducts the main game loop, triggering the race conduct process.</li>
+ * </ul>
+     */
     public void act() {
         conductRace();        
     }
@@ -55,6 +90,16 @@ public class HorseBetting extends Game
         raceInProgress = true;
         winningHorse = Greenfoot.getRandomNumber(numberOfHorses) + 1;
     }
+    
+    /**
+     * * <p><strong>int calculatePayout(int gamblerIndex)</strong></p>
+ * <ul>
+ *     <li><strong>@param gamblerIndex</strong> - Index of the gambler in the <code>gamblers</code> array.</li>
+ *     <li><strong>@return int</strong> - The calculated payout amount for the specified gambler based on the race outcome.</li>
+ *     <li>This method calculates the payout for a gambler based on their bet and the result of the race.</li>
+ * </ul>
+ * 
+     */
     
     public int calculatePayout(int gamblerIndex) {
         if (gamblerSelections[gamblerIndex] == -1 && winningHorse % 2 == 1) {
--- Casino Simulation/SlotMachines.java
+++ Casino Simulation/SlotMachines.java
@@ -1,7 +1,24 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 /**
- * The slot machines
- * Author: Dorsa Rohani
+ * <html>
+ * <body>
+ * <p><strong>SlotMachines</strong> is a <em>Game</em> subclass that simulates the operation of slot machines within a Greenfoot gaming environment.</p>
+ * <p>This class handles the logic and interface for slot machine games, including managing betting amounts, play cycles, random win calculations, and session management for players.</p>
+ * 
+ * <h3>Usage:</h3>
+ * <p>To use the <code>SlotMachines</code> class, instantiate it with an array of <code>SpotManager.Spot</code> objects representing the positions for the game. The class handles game sessions, deducting costs for playing, and calculating wins, making it a complete simulation of a slot machine experience.</p>
+ *
+ * <h3>Version Changes</h3>
+ * <ul>
+ *   <li>Initial Version: Basic functionality for slot machine simulation including cost deduction, win calculation, and session management.</li>
+ * </ul>
+ *
+ * <h3>Additional Information:</h3>
+ * <p>The <code>SlotMachines</code> class is intended for educational and simulation purposes within the Greenfoot framework. For a more comprehensive simulation, future enhancements could include diverse betting strategies, player profiles, and more sophisticated win calculation algorithms.</p>
+ * <p><strong>@author :</strong> Dorsa Rohani<br>
+ * <strong>@version :</strong> 1.2<br>
+ * </body>
+ * </html>
  */
 public class SlotMachines extends Game {
     private static final int cost = 5;
@@ -45,6 +62,17 @@ public class SlotMachines extends Game {
         gamblers[0].playMoneyEffect(-cost);
         HorizontalBar.casinoProfit += cost;
     }
+    
+    /**
+     * <p><strong>void winMoney()</strong></p>
+     * <ul>
+     *     <li>No parameters.</li>
+     *     <li>No return value.</li>
+     *     <li>Calculates and awards a random winning amount to the gambler.</li>
+     * </ul>
+     * 
+     */
+    
     public void winMoney() {
         //if (Greenfoot.getRandomNumber(2)==0) {
             actuallyWinningMoney=Greenfoot.getRandomNumber(500)+10;
@@ -55,7 +83,17 @@ public class SlotMachines extends Game {
        // }
     }
 
-    private void endGamblerSession() {
+    /**
+     *
+     * <p><strong>void endGamblerSession()</strong></p>
+     * <ul>
+     *     <li>No parameters.</li>
+     *     <li>No return value.</li>
+     *     <li>Ends the current gambler's session and resets the slot machine for the next player.</li>
+     * </ul>
+     * 
+     */
+    public void endGamblerSession() {
         gamblers[0].stopPlaying();
         gamblers[0] = null;
         //playCounter = 0; // reset play counter for next gambler
