87d3dc4e328412efe3b672668d46b317783a16a1
SoulEmp <Awesomez0legendz@gmail.com>
2023-11-26 11:27:18 -0500

    roulette coordiantes

--- Casino Simulation/CasinoWorld.java
+++ Casino Simulation/CasinoWorld.java
@@ -83,7 +83,7 @@ public class CasinoWorld extends World
         addObject(new SlotMachines(new SpotManager.Spot[]{new SpotManager.Spot(240,350,-20)}),240,325);
         addObject(new SlotMachines(new SpotManager.Spot[]{new SpotManager.Spot(320,350,-20)}),320,325);
         addObject(new SlotMachines(new SpotManager.Spot[]{new SpotManager.Spot(400,350,-20)}),400,325);
-        addObject(new Roulette(new SpotManager.Spot[]{new SpotManager.Spot(1150,290,-80), new SpotManager.Spot(1116,320,-45), new SpotManager.Spot(1068,335,-30), new SpotManager.Spot(1014,330,-40), new SpotManager.Spot(975,300,-60), new SpotManager.Spot(975,260,-120)}),1060,270);
+        addObject(new Roulette(new SpotManager.Spot[]{new SpotManager.Spot(1150,290,-80), new SpotManager.Spot(1120,325,-45), new SpotManager.Spot(1070,335,-30), new SpotManager.Spot(1014,330,-40), new SpotManager.Spot(990,310,-60), new SpotManager.Spot(975,280,-120)}),1060,270);
         addObject(new Poker(new SpotManager.Spot[]{new SpotManager.Spot(920,525,0), new SpotManager.Spot(940,440,55), new SpotManager.Spot(1000,435,50), new SpotManager.Spot(1050,435,50), new SpotManager.Spot(1130,460,75)}),1020,500);
         addObject(new Blackjack(new SpotManager.Spot[]{new SpotManager.Spot(80,445,70), new SpotManager.Spot(130,445,70), new SpotManager.Spot(180,445,70), new SpotManager.Spot(230,445,70), new SpotManager.Spot(270,480,0), new SpotManager.Spot(270,520,0)}),155,500);
         addObject(new HorseBetting(new SpotManager.Spot[]{new SpotManager.Spot(740,285,-80), new SpotManager.Spot(798,280,-80), new SpotManager.Spot(877,275,-80), new SpotManager.Spot(758+2*48,255,-80), new SpotManager.Spot(710+48,250,-80), new SpotManager.Spot(710,245,-80), new SpotManager.Spot(678,265,-80)}),780,196);
