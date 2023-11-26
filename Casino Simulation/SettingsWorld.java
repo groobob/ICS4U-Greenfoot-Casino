de70dfafd8c6237316ad92ff70c43b44c361d9d2
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-25 23:16:30 -0500

    add visible vals to sliders

--- Casino Simulation/SettingsWorld.java
+++ Casino Simulation/SettingsWorld.java
@@ -11,32 +11,52 @@ public class SettingsWorld extends World {
     private int variable3; // placeholder
     private int variable4; // placeholder
 
+    // Text objects to display the values
+    private Text valueText1;
+    private Text valueText2;
+    private Text valueText3;
+    private Text valueText4;
+
     public SettingsWorld() {    
         super(1200, 740, 1);
 
-        int initialValue1 = 25; // Example initial value for slider1
-        int initialValue2 = 50; // Example initial value for slider2
-        int initialValue3 = 75; // Example initial value for slider3
-        int initialValue4 = 100; // Example initial value for slider4
+        // Initial values for each slider
+        int initialValue1 = 25;
+        int initialValue2 = 50;
+        int initialValue3 = 75;
+        int initialValue4 = 100;
 
+        // Create sliders
         Slider slider1 = new Slider(1, 50, 550, 0, 100, initialValue1);
-        addObject(slider1, calculateSliderXPosition(slider1, initialValue1), 417);
-
         Slider slider2 = new Slider(2, 50, 550, 0, 100, initialValue2);
-        addObject(slider2, calculateSliderXPosition(slider2, initialValue2), 497);
-
         Slider slider3 = new Slider(3, 50, 550, 0, 100, initialValue3);
-        addObject(slider3, calculateSliderXPosition(slider3, initialValue3), 577);
-
         Slider slider4 = new Slider(4, 50, 550, 0, 100, initialValue4);
-        addObject(slider4, calculateSliderXPosition(slider4, initialValue4), 647);
 
+        // Add sliders and text to the world
+        addObject(slider1, calculateSliderXPosition(slider1, initialValue1), 417);
+        valueText1 = new Text(12, "Arial", String.valueOf(initialValue1));
+        addObject(valueText1, 680, 417); // Adjust position as needed
+
+        addObject(slider2, calculateSliderXPosition(slider2, initialValue2), 497);
+        valueText2 = new Text(12, "Arial", String.valueOf(initialValue2));
+        addObject(valueText2, 680, 497);
+
+        addObject(slider3, calculateSliderXPosition(slider3, initialValue3), 577);
+        valueText3 = new Text(12, "Arial", String.valueOf(initialValue3));
+        addObject(valueText3, 680, 577);
+
+        addObject(slider4, calculateSliderXPosition(slider4, initialValue4), 647);
+        valueText4 = new Text(12, "Arial", String.valueOf(initialValue4));
+        addObject(valueText4, 680, 647);
+
+        // Set labels
         showText("Settings", 600, 300);
         showText("Casino Target", 440, 417);
         showText("Ordinary Gambler Starting Money", 440, 497);
         showText("VIP Gambler Starting Money", 440, 577);
         showText("Roulette Style ", 440, 647);
 
+        // Initialize variables
         variable2 = initialValue2;
         variable3 = initialValue3;
         variable4 = initialValue4;
@@ -47,12 +67,23 @@ public class SettingsWorld extends World {
     }
 
     public void updateVar(int sliderID, int value) {
-        System.out.println(value);
         switch (sliderID) {
-            //case 1: UIManager.casinoProfit = value; break;
-            case 2: variable2 = value; break;
-            case 3: variable3 = value; break;
-            case 4: variable4 = value; break;
+            case 1:
+                valueText1.changeText(String.valueOf(value));
+                break;
+            case 2:
+                variable2 = value;
+                valueText2.changeText(String.valueOf(value));
+                break;
+            case 3:
+                variable3 = value;
+                valueText3.changeText(String.valueOf(value));
+                break;
+            case 4:
+                variable4 = value;
+                valueText4.changeText(String.valueOf(value));
+                break;
         }
     }
 }
+
