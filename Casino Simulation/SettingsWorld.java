870c7eb5b0258953de1bc289ecd605a03555f91e
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-26 16:03:18 -0500

    start button

--- Casino Simulation/SettingsWorld.java
+++ Casino Simulation/SettingsWorld.java
@@ -38,7 +38,7 @@ public class SettingsWorld extends World {
 
     public SettingsWorld() {    
         super(1200, 740, 1);
-        setBackground("settingsworldbg.png");
+        setBackground("settingsworld.png");
         
         // initial values
         casinoTarget = 10000; 
@@ -48,42 +48,44 @@ public class SettingsWorld extends World {
         ordinaryStartingMoney = 1;
         rouletteStyle = 38;
         numberOfHorses = 7;
+        slotsWinRate = 1;
+        vipGamblerStartingMoney = 5000;
 
-        Slider slider1 = new Slider(1, 708, 865, 10000, 1000000, casinoTarget);
-        Slider slider2 = new Slider(2, 708, 591, 1, 25, vipGamblerSpawnRate);
-        Slider slider3 = new Slider(3, 708, 549, 5000, 10000, vipGamblerStartingMoney);
-        Slider slider4 = new Slider(4, 708, 637, 1, 25, cheaterGamblerSpawnRate);
-        Slider slider5 = new Slider(5, 708, 503, 1, 5000, ordinaryStartingMoney);
-        Slider slider6 = new Slider(6, 702, 269, 1, 99, slotsWinRate);
-        Slider slider7 = new Slider(7, 708, 312, 7, 20, numberOfHorses);
+        Slider slider1 = new Slider(1, 708, 822, 10000, 1000000, casinoTarget);
+        Slider slider2 = new Slider(2, 708, 822, 1, 25, vipGamblerSpawnRate);
+        Slider slider3 = new Slider(3, 708, 822, 5000, 10000, vipGamblerStartingMoney);
+        Slider slider4 = new Slider(4, 708, 822, 1, 25, cheaterGamblerSpawnRate);
+        Slider slider5 = new Slider(5, 708, 822, 1, 5000, ordinaryStartingMoney);
+        Slider slider6 = new Slider(6, 708, 822, 1, 99, slotsWinRate);
+        Slider slider7 = new Slider(7, 708, 822, 7, 20, numberOfHorses);
         
         addObject(slider1, calculateSliderXPosition(slider1, casinoTarget), 219);
         valueText1 = new Text(12, "Arial", String.valueOf(casinoTarget));
-        addObject(valueText1, 905, 217);
+        addObject(valueText1, 877, 223);
 
         addObject(slider2, calculateSliderXPosition(slider2, vipGamblerSpawnRate), 591);
         valueText2 = new Text(12, "Arial", String.valueOf(vipGamblerSpawnRate));
-        addObject(valueText2, 905, 591);
+        addObject(valueText2, 893, 591);
 
         addObject(slider3, calculateSliderXPosition(slider3, vipGamblerStartingMoney), 549);
         valueText3 = new Text(12, "Arial", String.valueOf(vipGamblerStartingMoney));
-        addObject(valueText3, 905, 549);
+        addObject(valueText3, 885, 549);
 
         addObject(slider4, calculateSliderXPosition(slider4, cheaterGamblerSpawnRate), 637);
         valueText4 = new Text(12, "Arial", String.valueOf(cheaterGamblerSpawnRate));
-        addObject(valueText4, 905, 637);
+        addObject(valueText4, 893, 637);
         
         addObject(slider5, calculateSliderXPosition(slider5, ordinaryStartingMoney), 503);
         valueText5 = new Text(12, "Arial", String.valueOf(ordinaryStartingMoney));
-        addObject(valueText5, 905, 503);
+        addObject(valueText5, 893, 503);
         
         addObject(slider6, calculateSliderXPosition(slider6, slotsWinRate), 269);
         valueText6 = new Text(12, "Arial", String.valueOf(slotsWinRate));
-        addObject(valueText6, 905, 269);
+        addObject(valueText6, 893, 269);
         
         addObject(slider7, calculateSliderXPosition(slider7, numberOfHorses), 312);
         valueText7 = new Text(12, "Arial", String.valueOf(numberOfHorses));
-        addObject(valueText7, 905, 312);
+        addObject(valueText7, 893, 312);
 
         //showText("Settings", 600, 300);
         //showText("Casino Target", 440, 417);
@@ -96,7 +98,7 @@ public class SettingsWorld extends World {
         
         
         //button
-        startButton = new Button("START", 90, 255, 0, 0, 0, 20, 147);
+        startButton = new Button("START", 40, 255, 255, 255, 255, 0, 0);
         rouletteEuropean = new Button("European ", 30, 255, 0, 0, 0, 20, 147);
         rouletteAmerican = new Button("American ", 30, 255, 0, 0, 0, 20, 147);
         rouletteSands = new Button("Sands",30, 255, 0, 0, 0, 20, 147);
@@ -104,7 +106,7 @@ public class SettingsWorld extends World {
         //title = new Button("Title", 150, 255, 255, 255, 255, 20, 147);
         
         // Add buttons to the world
-       // addObject(startButton, 625, 200);
+       addObject(startButton, 623, 719);
         //addObject(rouletteEuropean, 700, 250);
         //addObject(rouletteAmerican, 700, 270);
         //addObject(rouletteSands, 700, 290);
