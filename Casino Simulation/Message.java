b97f88f3f98c4e72ff9eefa1f88cf8b2d6e43e77
Dorsa <85207154+DorsaRoh@users.noreply.github.com>
2023-11-26 23:02:33 -0500

    message doc

--- Casino Simulation/Message.java
+++ Casino Simulation/Message.java
@@ -6,8 +6,13 @@ import greenfoot.*;
  * <p>The <em>Message</em> class represents a message displayed on the screen with customizable text, color, size, lifespan, levitating speed, and fading effect.</p>
  * <h2>Class Attributes:</h2>
  * <ul>
- *     <li><strong>up:</strong> A boolean flag to manage key press states.</li>
+ *     <li><strong>lifespan (int):</strong> The remaining lifespan of the message in acts.</li>
+ *     <li><strong>startingLifespan (int):</strong> The initial lifespan of the message in acts.</li>
+ *     <li><strong>leviatingSpeed (int):</strong> The speed at which the message levitates.</li>
+ *     <li><strong>isNew (boolean):</strong> A boolean indicating if the message is new (not yet added to the world).</li>
+ *     <li><strong>value (String):</strong> The text content of the message.</li>
  * </ul>
+
  */
 
 public class Message extends Actor {
